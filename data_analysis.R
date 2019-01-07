abc49cc02bf3572430c921f1510a9269f5334145
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-01-07 13:59:43 +0700

    Stuck for now, looking for another time to continue

--- data_analysis.R
+++ data_analysis.R
@@ -1,38 +1,40 @@
 library(dplyr)
 library(sqldf)
+library(data.table)
 
 ## Read data
-df_AWS_Payment_201805 <- read.csv("./data_AWS/AWS_Payment_201805.csv")
-df_AWS_Payment_201806 <- read.csv("./data_AWS/AWS_Payment_201806.csv")
-df_AWS_Payment_201807 <- read.csv("./data_AWS/AWS_Payment_201807.csv")
-df_AWS_Payment_201808 <- read.csv("./data_AWS/AWS_Payment_201808.csv")
-df_AWS_Payment_201809 <- read.csv("./data_AWS/AWS_Payment_201809.csv")
-df_AWS_Payment_201810 <- read.csv("./data_AWS/AWS_Payment_201810.csv")
+AWS_Payment_201805 <- fread("./data_AWS/AWS_Payment_201805.csv")
+AWS_Payment_201806 <- fread("./data_AWS/AWS_Payment_201806.csv")
+AWS_Payment_201807 <- fread("./data_AWS/AWS_Payment_201807.csv")
+AWS_Payment_201808 <- fread("./data_AWS/AWS_Payment_201808.csv")
+AWS_Payment_201809 <- fread("./data_AWS/AWS_Payment_201809.csv")
+AWS_Payment_201810 <- fread("./data_AWS/AWS_Payment_201810.csv")
 
 ##############################################################################################################################
 ##
 ##############################################################################################################################
 
 ## Merge all 6 AWS Payment datasets
-AWSPayment <- rbind(df_AWS_Payment_201805, df_AWS_Payment_201806, df_AWS_Payment_201807,
-                      df_AWS_Payment_201808, df_AWS_Payment_201809, df_AWS_Payment_201810)
-
+Merged_AWS_Payment <- rbind(AWS_Payment_201805, AWS_Payment_201806, AWS_Payment_201807,
+                      AWS_Payment_201808,AWS_Payment_201809, AWS_Payment_201810)
 
 # remove individual data frames to save memory
-rm(df_AWS_Payment_201805, df_AWS_Payment_201806, df_AWS_Payment_201807,
-    df_AWS_Payment_201808, df_AWS_Payment_201809, df_AWS_Payment_201810)
+rm(AWS_Payment_201805, AWS_Payment_201806, AWS_Payment_201807,
+    AWS_Payment_201808, AWS_Payment_201809, AWS_Payment_201810)
 
 ##############################################################################################################################
 ##
 ##############################################################################################################################
 
+str(Merged_AWS_Payment)
+dim(Merged_AWS_Payment[InvoiceID == "136209072"])
+
 ## Extract only the measurements
-
-# determine columns of data set to keep based on column name
-columnsToKeep <- grepl("InvoiceID|PayerAccountId|LinkedAccountId", colnames(AWSPayment))
-
-# keep data in these columns only
-AWSPayment <- AWSPayment[, columnsToKeep]
+# # determine columns of data set to keep based on column name
+# columnsToKeep <- grepl("InvoiceID|PayerAccountId|LinkedAccountId", colnames(AWSPayment))
+# 
+# # keep data in these columns only
+# AWSPayment <- AWSPayment[, columnsToKeep]
 
 ##############################################################################################################################
 ##
@@ -40,7 +42,7 @@ AWSPayment <- AWSPayment[, columnsToKeep]
 
 ## From the data set above, creates a second, independent tidy data set
 ## with the average of each variable for each activity and each subject.
-AWSPayment_data_analysis <- AWSPayment
-write.csv(AWSPayment_data_analysis, "AWSPayment_data_analysis.csv")
+AWS_Payment_data_analysis <- Merged_AWS_Payment
+write.csv(AWS_Payment_data_analysis, "AWSPayment_data_analysis.csv")
 
 
